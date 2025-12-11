2121466ad878a47bfe521fb2d48349a01017bda9
unknown <huznigarane@gmail.com>
2025-12-11 09:30:10 +0300

    Update QR code to point to compressed PDF and add new PDF file

--- src/components/QRSection.tsx
+++ src/components/QRSection.tsx
@@ -3,8 +3,8 @@ import { QRCodeSVG } from "qrcode.react";
 const QRSection = () => {
   // Local Booklet PDF URL
   const pdfUrl = typeof window !== "undefined" 
-    ? `${window.location.origin}/docs/manifesto-yetu-booklet.pdf`
-    : "/docs/manifesto-yetu-booklet.pdf";
+    ? `${window.location.origin}/docs/Manifesto%20Yetu%20A5%20copy_compressed.pdf`
+    : "/docs/Manifesto%20Yetu%20A5%20copy_compressed.pdf";
 
   return (
     <div className="flex flex-col items-center justify-center gap-8 p-8 lg:p-12">
